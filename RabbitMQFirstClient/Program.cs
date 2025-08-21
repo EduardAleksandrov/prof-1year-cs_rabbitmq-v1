ffee8b6b66c6590e99f32979625ca47d180454ba
Eduard Aleksandrov <eduard.ale@list.ru>
2025-08-21 18:25:36 +0300

    Два случая разобраны

---
+++ RabbitMQFirstClient/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "myuser", Password = "mypassword" };
+        using (var connection = await factory.CreateConnectionAsync())
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            await channel.QueueDeclareAsync(queue: "hello",
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (sender, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+                // Acknowledge the message
+                await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+            };
+            await channel.BasicConsumeAsync(queue: "hello",
+                                            autoAck: false,
+                                            consumer: consumer);
+            Console.WriteLine("Waiting for messages...");
+            Console.ReadLine();
+        }
+        // Example two
+        await JsonExample();
+    }
+    private static async Task JsonExample()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "myuser", Password = "mypassword" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        // Declare (or check) the queue to consume from
+        await channel.QueueDeclareAsync(
+            queue: "orders",
+            durable: true, // must match the producer's queue settings
+            exclusive: false, // can be used by other connections
+            autoDelete: false, // don’t delete when the last consumer disconnects
+            arguments: null);
+
+        // Define a consumer and start listening
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += async (sender, eventArgs) =>
+        {
+            byte[] body = eventArgs.Body.ToArray();
+            string message = Encoding.UTF8.GetString(body);
+            var orderPlaced = JsonSerializer.Deserialize<OrderPlaced>(message);
+
+            Console.WriteLine($"Received: OrderPlaced - {orderPlaced?.OrderId} - {orderPlaced?.Total} - {orderPlaced?.CreatedAt}");
+
+            // Acknowledge the message
+            await ((AsyncEventingBasicConsumer)sender)
+                .Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+
+            await Console.Out.WriteLineAsync("Отправлено Ack");
+        };
+        await channel.BasicConsumeAsync("orders", autoAck: false, consumer);
+
+        Console.WriteLine("Waiting for messages...");
+        Console.ReadLine();
+    }
+}
+
+public class OrderPlaced
+{
+    public Guid OrderId { get; set; } // Свойство для идентификатора заказа
+    public double Total { get; set; } // Свойство для общей суммы заказа
+    public DateTime CreatedAt { get; set; } // Свойство для даты и времени создания заказа
+
+}
---
+++ RabbitMQFirstServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "myuser", Password = "mypassword" };
+        using (var connection = await factory.CreateConnectionAsync())
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            await channel.QueueDeclareAsync(queue: "hello",
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            string message = "Привет - Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: "",
+                                            routingKey: "hello",
+                                            mandatory: true, // fail if the message can’t be routed
+                                            basicProperties: new BasicProperties { Persistent = true }, // message will be saved to disk,
+                                            body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+        }
+        // Example two
+        await JsonExample();
+    }
+
+    private static async Task JsonExample()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "myuser", Password = "mypassword" };
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        // Ensure the queue exists (create it if not already there)
+        await channel.QueueDeclareAsync(
+            queue: "orders",
+            durable: true, // save to disk so the queue isn’t lost on broker restart
+            exclusive: false, // can be used by other connections
+            autoDelete: false, // don’t delete when the last consumer disconnects
+            arguments: null);
+
+        // Create a message
+        var orderPlaced = new
+        {
+            OrderId = Guid.NewGuid(),
+            Total = 99.99,
+            CreatedAt = DateTime.UtcNow
+        };
+        var message = JsonSerializer.Serialize(orderPlaced);
+        var body = Encoding.UTF8.GetBytes(message);
+
+        // Publish the message
+        await channel.BasicPublishAsync(
+            exchange: string.Empty, // default exchange
+            routingKey: "orders",
+            mandatory: true, // fail if the message can’t be routed
+            basicProperties: new BasicProperties { Persistent = true }, // message will be saved to disk
+            body: body);
+
+        Console.WriteLine($"Sent: {message}");
+    }
+}
